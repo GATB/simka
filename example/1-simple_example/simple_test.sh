22ee664bfc27a60d23471c44d0688ef4257d6dec
gbenoit <gaetan.benoit@inria.fr>
2016-11-22 16:46:11 +0000

    1-simple_example

--- example/1-simple_example/simple_test.sh
+++ example/1-simple_example/simple_test.sh
@@ -3,19 +3,19 @@
 
 # look for simka binary. In devel mode, it's in ../build/bin directory.
 # In production mode, it's in ../bin directory.
-if [ -f "../bin/simka" ]
-then
- bindir="../bin"
-elif [ -f "../build/bin/simka" ]
-then
- bindir="../build/bin"
-else
- echo "could not find a compiled simka binary"
- exit 1
-fi
+#if [ -f "../../scripts/simka2/bin/simka" ]
+#then
+# bindir="../bin"
+#elif [ -f "../build/bin/simka" ]
+#then
+# bindir="../build/bin"
+#else
+# echo "could not find a compiled simka binary"
+# exit 1
+#fi
 
 # run simka
-command="$bindir/simka -in ../example/simka_input.txt -out output/ -out-tmp temp_output"
+command="python ../scripts/simka2/simka.py -in ../example/1-simple_example/simka_input.txt -out output/ -out-tmp temp_output"
 #printf "$command\n\n"
 
 $command
@@ -37,4 +37,4 @@ rm -rf temp_output
 printf "\nCommand used:\n" 
 printf "\t$command\n"
 printf "Command for creating heatmaps and dendrogram:\n"
-printf "\tpython ../scripts/create_heatmaps.py output/\n"
+printf "\tpython ../../scripts/create_heatmaps.py output/\n"
