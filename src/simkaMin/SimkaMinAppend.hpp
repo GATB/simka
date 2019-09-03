9007210c7ca45943b313bf0033b8846468cc908a
Claire Lemaitre <claire.lemaitre@inria.fr>
2019-09-03 16:56:47 +0200

    adding licence info

--- src/simkaMin/SimkaMinAppend.hpp
+++ src/simkaMin/SimkaMinAppend.hpp
@@ -1,9 +1,22 @@
-/*
- * SimkaMinAppend.h
+/*****************************************************************************
+ *   SimkaMin: Fast kmer-based method for estimating the similarity between numerous metagenomic datasets
+ *   A tool from the GATB (Genome Assembly Tool Box)
+ *   Copyright (C) 2019  INRIA
+ *   Authors: G.Benoit
  *
- *  Created on: 7 déc. 2017
- *      Author: gbenoit
- */
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
 
 #ifndef SIMKA1_4_SRC_SIMKAMIN_SIMKAMINAPPEND_HPP_
 #define SIMKA1_4_SRC_SIMKAMIN_SIMKAMINAPPEND_HPP_
