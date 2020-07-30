8bfbc31092644d5b34546b4fb4a8657d968d40c3
Charles Deltel <charles.deltel@inria.fr>
2020-07-30 16:48:23 +0200

    Update analysis.sh (change echo in echo_stderr)

--- scripts/sonarqube_diags/analysis.sh
+++ scripts/sonarqube_diags/analysis.sh
@@ -2,15 +2,17 @@
 # SonarQube server.
 #!/bin/bash
 
-echo "===> Launching analysis script (to prepare SonarQube diagnostics)..."
+echo_stderr() { echo "$@" 1>&2; }
+
+echo_stderr "===> Launching analysis script (to prepare SonarQube diagnostics)..."
 
 ####################################################################################################
 # CppCheck and RATS analysis
 ####################################################################################################
-echo "===> Launching CppCheck analysis..."
+echo_stderr "===> Launching CppCheck analysis..."
 cppcheck -j2 -f --enable=all --suppress=missingIncludeSystem --xml-version=2 src/ &> simka-cppcheck.xml
 
-echo "===> Launching RATS analysis..."
+echo_stderr "===> Launching RATS analysis..."
 rats -w 3 --xml src > simka-rats.xml
 
 ####################################################################################################
@@ -20,18 +22,18 @@ mkdir build
 cd build
 
 # compilation options
-echo "===> Compilation options"
+echo_stderr "===> Compilation options"
 CFLAGS="--coverage \
 	-fPIC -fdiagnostics-show-option \
 	-Wall -Wunused-parameter -Wundef -Wno-long-long \
 	-Wsign-compare -Wmissing-prototypes -Wstrict-prototypes -Wcomment \
 	-pedantic -g"
 LDFLAGS="--coverage"
-echo "CFLAGS: $CFLAGS"
-echo "LDFLAGS: $LDFLAGS"
+echo_stderr "CFLAGS: $CFLAGS"
+echo_stderr "LDFLAGS: $LDFLAGS"
 
 # launch cmake
-echo "===> Launching cmake with scan-build..."
+echo_stderr "===> Launching cmake with scan-build..."
 
 scan-build -v -plist --intercept-first --analyze-headers -o analyzer_reports \
       cmake .. -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
@@ -40,7 +42,7 @@ scan-build -v -plist --intercept-first --analyze-headers -o analyzer_reports \
       -DCMAKE_CXX_OUTPUT_EXTENSION_REPLACE=ON &> simka-scan-build-cmake.log
 
 # launch make
-echo "===> Launching make with scan-build..." 
+echo_stderr "===> Launching make with scan-build..." 
 time scan-build -v -plist --intercept-first --analyze-headers -o analyzer_reports \
    make -j &> simka-scan-build-make.log
 #  make -j
@@ -53,7 +55,7 @@ mv analyzer_reports ..
 ####################################################################################################
 # Clang-tidy analysis
 ####################################################################################################
-echo "===> Launching run-clang-tidy..."
+echo_stderr "===> Launching run-clang-tidy..."
 test -f compile_commands.json || echo "Warning, compilation database missing"
 run-clang-tidy-3.8.py -checks='*'  -p . -j4 > simka-clang-tidy-report.log
 
@@ -64,17 +66,17 @@ mv compile_commands.json ..
 # Coverage analysis
 ####################################################################################################
 
-echo "===> Launching lcov (initial)..."
+echo_stderr "===> Launching lcov (initial)..."
 
 # run initial/baseline lcov
 lcov --capture --initial --directory . --output-file simka_coverage_base.info
 
 # run tests
-echo "===> Launching a simple test run..."
+echo_stderr "===> Launching a simple test run..."
 ../example/simple_test.sh
 
 # run lcov again after tests complete
-echo "===> Launching lcov (after test completion)..."
+echo_stderr "===> Launching lcov (after test completion)..."
 lcov --capture --directory . --output-file simka_coverage_test.info
 
 # combine lcov tracefiles
@@ -87,13 +89,13 @@ lcov --extract simka_coverage_total.info '/home/gitlab/simka/src/*' \
     --output-file simka_coverage_total_filtered.info
 
 # generate the html lcov page that can be published
-echo "===> Generating coverage html pages..."
+echo_stderr "===> Generating coverage html pages..."
 genhtml -o coverage-html simka_coverage_total_filtered.info
 mv coverage-html ..
 
 # convert the lcov report to an xml format convertible with SonarQube
-echo "===> Generating coverage report for SonarQube..."
+echo_stderr "===> Generating coverage report for SonarQube..."
 lcov_cobertura.py simka_coverage_total_filtered.info --output gcov.xml
 mv gcov.xml ..
 
-echo "===> Done..."
+echo_stderr "===> Done..."
