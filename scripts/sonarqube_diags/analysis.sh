2990d3bea24969312c5fcd27fedc95bd96223aa1
Charles Deltel <charles.deltel@inria.fr>
2020-09-03 17:28:30 +0200

    Tune SonarQube diagnostics

--- scripts/sonarqube_diags/analysis.sh
+++ scripts/sonarqube_diags/analysis.sh
@@ -1,6 +1,7 @@
 # This script launches several diagnostics, whose results will be uploaded to the
 # SonarQube server.
 #!/bin/bash
+set -xv 
 
 echo_stderr() { echo "$@" 1>&2; }
 
@@ -9,11 +10,22 @@ echo_stderr "===> Launching analysis script (to prepare SonarQube diagnostics)..
 ####################################################################################################
 # CppCheck and RATS analysis
 ####################################################################################################
+
 echo_stderr "===> Launching CppCheck analysis..."
-cppcheck -j2 -f --enable=all --suppress=missingIncludeSystem --xml-version=2 src/ &> simka-cppcheck.xml
+# see e.g. https://sonarqube.inria.fr/pages/documentation.html#org4575413
+
+export CPPCHECK_INCLUDES="-Icore -Iminikc -IsimkaMin"
+export SOURCES_TO_ANALYZE="src"
+export SOURCES_TO_EXCLUDE=     # ex. "-isrc/beta"
+export DEFINITIONS=            # -D
+
+cppcheck -v -f --language=c++ --platform=unix64 --enable=all --suppress=missingIncludeSystem --xml --xml-version=2 \
+    ${DEFINITIONS} ${CPPCHECK_INCLUDES} ${SOURCES_TO_EXCLUDE} ${SOURCES_TO_ANALYZE} \
+    2> simka-cppcheck.xml
 
 echo_stderr "===> Launching RATS analysis..."
-rats -w 3 --xml src > simka-rats.xml
+export SOURCES_TO_ANALYZE="src"
+rats -w 3 --xml ${SOURCES_TO_ANALYZE} > simka-rats.xml
 
 ####################################################################################################
 # Compile the code
@@ -56,11 +68,12 @@ mv analyzer_reports ..
 ####################################################################################################
 # Clang-tidy analysis
 ####################################################################################################
-echo_stderr "===> Launching run-clang-tidy..."
+echo_stderr "===> Launching clang-tidy..."
 test -f compile_commands.json || echo "Warning, compilation database missing"
-run-clang-tidy-3.8.py -checks='*'  -p . -j4 > simka-clang-tidy-report.log
-
-mv simka-clang-tidy-report.log ..
+cd ..
+#clang-tidy  -p build/  $(find src/ -name *.cpp) -checks='*' > simka-clang-tidy-report.log
+clang-tidy  -p build/  $(find src/ -name *.cpp) -checks=-*,clang-analyzer-*,cppcoreguidelines-* > simka-clang-tidy-report.log
+cd -    # back in build/ again
 mv compile_commands.json ..
 
 ####################################################################################################
@@ -74,7 +87,7 @@ lcov --capture --initial --directory . --output-file simka_coverage_base.info
 
 # run tests
 echo_stderr "===> Launching a simple test run..."
-../example/simple_test.sh
+../example/simple_test.sh -max-count 2 -max-merge 4 -nb-cores 4 -max-memory 3000
 
 # run lcov again after tests complete
 echo_stderr "===> Launching lcov (after test completion)..."
@@ -86,7 +99,7 @@ lcov --add-tracefile simka_coverage_base.info \
     --output-file simka_coverage_total.info
 
 # extract useful data
-lcov --extract simka_coverage_total.info '/home/gitlab/simka/src/*' \
+lcov --extract simka_coverage_total.info '*simka/src/*' \
     --output-file simka_coverage_total_filtered.info
 
 # generate the html lcov page that can be published
@@ -96,7 +109,7 @@ mv coverage-html ..
 
 # convert the lcov report to an xml format convertible with SonarQube
 echo_stderr "===> Generating coverage report for SonarQube..."
-lcov_cobertura.py simka_coverage_total_filtered.info --output gcov.xml
-mv gcov.xml ..
+cd ..
+lcov_cobertura.py build/simka_coverage_total_filtered.info --output gcov.xml --base-dir src/
 
 echo_stderr "===> Done..."
